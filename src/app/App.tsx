ba1e56a141e08abd69648b5898281db25788458a
joshuapeter-tolu <177851580+joshuapeter-tolu@users.noreply.github.com>
2024-11-10 11:30:52 -0800

    Product list created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const LOGO_URI = `http://www.palaisdetokyo.com/sites/default/files/aiaiai_logo_circle_new_1.png`;
+const getImageUri = (id: any) => `https://aiaiai.dk/images/front/${id}_m.png`;
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hi Peter</Text>
       <StatusBar style="auto" />
     </View>
   );
