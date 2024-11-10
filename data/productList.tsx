ebf48bf9c052abf3a6cdd87aff9642aa00638c92
joshuapeter-tolu <177851580+joshuapeter-tolu@users.noreply.github.com>
2024-11-10 14:27:03 -0800

    project completed

--- data/productList.tsx
+++ data/productList.tsx
@@ -1,39 +1,43 @@
 export const PRODUCT_LIST = [
     {
-      id: "s02h02e02c02",
+      id: 1,
       title: "TMA-2",
       subtitle: "DJ PRESET",
       description:
         "This configuration is based on the original TMA-1 DJ, which is the preferred choice of a range of acclaimed DJs.",
       price: "200€",
-      bg: "#16CDC1"
+        bg: "#16CDC1",
+       imageUrl: "https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/KeyVisual_3500x3500-vOSLD9gb-large.png"
     },
     {
-      id: "s74h02e02c74",
+      id: 2,
       title: "TMA-2",
       subtitle: "ED BANGER EDITION",
       description:
         "This combination provides a very heavy and powerful bass. Recommended for bass lovers and those who like it loud. Limited edition of 300.",
       price: "240€",
-      bg: "#bbb"
+        bg: "#bbb",
+       imageUrl: "https://images.prismic.io/aiaiai/Zg0QD8t2UUcvBWZ-_tma-2-ninja-tune-edi-0FXQZGBF-large.png?auto=format%2Ccompress&rect=0%2C0%2C700%2C700&w=1400&h=1400"
     },
     {
-      id: "s04h71e05c71",
+      id: 3,
       title: "TMA-2",
       subtitle: "YOUNG GURU PRESET",
       description:
         "This configuration provides open, vibrant sound with good bass and treble. Wide sound stage and medium isolation.",
       price: "260€",
-      bg: "palevioletred"
+        bg: "palevioletred",
+       imageUrl: "https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/product-image-t_Kftsxc-large.png"
     },
     {
-      id: "s03h03e04c02",
+      id: 4,
       title: "TMA-2",
       subtitle: "STUDIO PRESET",
       description:
         "This configuration provides a warm sound and it is good for extended listening. Great bass and added energy in the lower mid range.",
       price: "225€",
-      bg: "#629BF0"
+        bg: "#629BF0",
+        imageUrl: "https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/Aiaiai_TMA2_Preset_D-LYTDMJcf-large.png"
     }
   ];
   
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,203 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { PRODUCT_LIST } from "../../data/productList";
+import { useRef } from "react";
+import Svg, { Defs, RadialGradient, Stop, Rect } from 'react-native-svg';
+const { width, height } = Dimensions.get("window");
 
-
-const LOGO_URI = `http://www.palaisdetokyo.com/sites/default/files/aiaiai_logo_circle_new_1.png`;
 const getImageUri = (id: any) => `https://aiaiai.dk/images/front/${id}_m.png`;
 
 export default function App() {
+  const _scrollX = useRef(new Animated.Value(0)).current;
+
+
+  const renderItem = (item: any, i: any) => {
+
+    const inputRange = [
+      (i - 2) * width,
+      (i - 1) * width,
+      i * width,
+      (i + 1) * width,
+    ];
+
+    const imageScale = _scrollX.interpolate({
+      inputRange,
+      outputRange: [1, .4, 1, .4],
+      extrapolate:"clamp"
+    });
+
+    const imageOpacity = _scrollX.interpolate({
+      inputRange,
+      outputRange: [1, 0.2, 1, 0.2],
+    })
+
+
+    return (
+      <View key={item.id} style={[styles.container, styles.item]}>
+        <Animated.Image
+          source={{ uri: item.imageUrl}}
+          style={[styles.image, {
+            transform: [{ scale: imageScale }],
+            opacity: imageOpacity,
+          }]}
+          onError={() => console.log(`Error loading image for id: ${item.id}`)}
+          fadeDuration={0}
+        />
+        <Animated.View style={[styles.metaContainer, {opacity:imageOpacity}]}>
+        <Text style={[styles.font, styles.title]}>{item.title}</Text>
+          <Text style={[styles.font, styles.subtitle]}>{item.subtitle}</Text>
+          <Text style={[styles.font, styles.description]}>
+            {item.description}
+          </Text>
+          <Text style={[styles.font, styles.price]}>{item.price}</Text>
+        </Animated.View>
+         {_renderRadialGradient(item.bg, i)}
+      </View>
+    ); 
+  };
+
+  const _renderRadialGradient = (color: any, i: any) => {
+
+    const inputRange = [
+      (i - 2) * width,
+      (i - 1) * width,
+      i * width,
+      (i + 1) * width,
+    ];
+
+
+
+const rotate = _scrollX.interpolate({
+  inputRange,
+  outputRange: ['0deg', '-15deg', '0deg','15deg'],
+})
+
+    const translateX = _scrollX.interpolate({
+      inputRange,
+      outputRange: [0, width, 0, -width],
+    })
+
+    const opacity = _scrollX.interpolate({
+      inputRange,
+      outputRange: [1, .5, 1, .5],
+    })
+
+
+
+    return (
+      <Animated.View style={[styles.svgContainer, {
+        transform: [{ rotate }, { translateX }, {scale:1.3}],
+        opacity: opacity,
+      }]}>
+         <Svg height={height} width={width}>
+      <Defs>
+        <RadialGradient id="gradient" cx="50%" cy="35%" r="60%" gradientUnits="userSpaceOnUse">
+          <Stop offset="0%" stopColor= "#fff" stopOpacity="1"/>
+          <Stop offset="100%" stopColor= {color} stopOpacity="1" />
+        </RadialGradient>
+      </Defs>
+      <Rect x="0" y="0" width={width} height={height} fill="url(#gradient)" fillOpacity={0.9}/>
+    </Svg>
+     </Animated.View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hi Peter</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={require("../../assets/logo.png")}
+        style={styles.logo}
+      />
+      <Animated.ScrollView
+        pagingEnabled
+        scrollEventThrottle={16}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: _scrollX } } }],
+          { useNativeDriver: true } 
+        )}
+        contentContainerStyle={styles.contentContainer}
+      >
+        {PRODUCT_LIST.map(renderItem)}
+      </Animated.ScrollView>
+      <StatusBar hidden />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  contentContainer: {
+    alignItems: "center",
+    justifyContent:"center",
+  },
+  item: {
+    width,
+    height,
+    alignItems:"center"
+  },
+  font: {
+    fontFamily: "menlo",
+    color:"#222"
+  },
+  metaContainer: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "transparent",
+    padding: 15
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "900",
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "900",
+  },
+  description: {
+    fontSize: 16,
+    marginVertical: 15,
+    textAlign: "center",
+  },
+  price: {
+    fontSize: 45,
+    fontWeight: "400",
+  },
+  logo: {
+    position: "absolute",
+    top:60,
+    width: width/7,
+    height: width/7,
+    resizeMode: "contain",
+    zIndex: 1000
+  },
+  image: {
+    width: width * .95,
+    height: width * .95,
+    resizeMode: "contain",
+  },
+  svgContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: width,
+    height: height,
+    zIndex: -2,
+    backgroundColor: "transparent",
+  }
 });
